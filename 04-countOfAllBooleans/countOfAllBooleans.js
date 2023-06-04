3a9dae66f088a7f9a479c23c70a26086dfb1c189
ericz02 <ezheng0720@gmail.com>
2023-06-04 17:36:33 -0400

    countOfAllBooleans done

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,16 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+
+  let count = 0; // Initialize a variable count to keep track of the number of boolean values
+
+  // Iterate over each element in the array
+  for (let i = 0; i < arr.length; i++) {
+    // Check if the current element is equal to true or false
+    if (arr[i] === true || arr[i] === false) {
+      count++; // If the current element is a boolean, increment the count by 1
+    }
+  }
+
+  return count; // Return the total count of boolean values in the array
 }
 
 // Do not edit this line;
