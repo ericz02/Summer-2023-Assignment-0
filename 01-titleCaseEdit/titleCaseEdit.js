41e0e9793665d1cd8d01b49434cb1899b77dbc9c
ericz02 <ezheng0720@gmail.com>
2023-06-04 00:11:32 -0400

    titleCaseEdit passed case test

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,16 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  
+  // title is split into an array of words using split() method, each word is seperated by a space character
+  const titleSplit = title.split(" ")
+
+  // iterate over each word in `titleSplit` array 
+  for (let i = 0; i < titleSplit.length; i++)
+
+    // capitalize the first character of each word and concatenate it with the remaining characters
+    titleSplit[i] = titleSplit[i].charAt(0).toUpperCase() + titleSplit[i].slice(1);
+
+    // join the modified words back into a string with spaces in between
+    return titleSplit.join(' ')
 }
 
 // Do not edit this line;
