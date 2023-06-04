ed1da1be89842c62b385560b34575645bde966ba
ericz02 <ezheng0720@gmail.com>
2023-06-04 17:54:46 -0400

    countOfAllBooleansAndStrings done

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,17 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+
+  let count = 0;
+
+  for (let  i = 0; i < arr.length; i++) {
+
+    type = typeof arr[i]
+
+    if (type === "string" || type === "boolean") {
+      count++
+    }
+  }
+  
+  return count;
 }
 
 // Do not edit this line;
