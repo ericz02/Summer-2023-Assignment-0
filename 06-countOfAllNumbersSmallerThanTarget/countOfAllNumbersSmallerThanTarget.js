2a0a0eaf3b5d59696aac79d55519a9842529a5e0
ericz02 <ezheng0720@gmail.com>
2023-06-07 00:46:37 -0400

    6, 7, 8, 9 pass cases

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,10 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+
+  // Use filter to create a new array with elements smaller than the target, then get the length of the filtered array
+  const count = nums.filter(num => num < target).length;
+  // Return the count of elements smaller than the target
+  return count; 
+
 }
 
 // Do not edit this line;
