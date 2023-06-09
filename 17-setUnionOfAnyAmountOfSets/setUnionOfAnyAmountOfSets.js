ab6db19019c0db9c58e5c0987694c45fa6017cb2
ericz02 <ezheng0720@gmail.com>
2023-06-08 22:14:44 -0400

    17 done

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,21 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+
+  let u = new Set();
+
+  for (let i = 0; i < args.length; i++) {
+
+    for (const item of args[i]) {
+
+      if (!u.has(item)) {
+        u.add(item)
+      }
+      
+    }
+
+  }
+
+  return u;
+  
 }
 
 // Do not edit this line;
